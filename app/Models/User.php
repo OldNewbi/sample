2aa1f989a70f410f98cf951a4e9c58660d9e8f13
luozong <it004@ferex.com.cn>
2018-03-02 09:16:47 +0000

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,11 @@ class User extends Authenticatable
         $hash=md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+    //生产激活令牌
+    public static function boot(){
+        parent::boot();
+        static::creating(function($user){
+            $user->activation_token=str_random(30);
+        });
+    }
 }
