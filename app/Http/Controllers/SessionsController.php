564f0a3d8080c49e76e55b0277d605b503a6ea1d
luozong <it004@ferex.com.cn>
2018-03-02 06:24:08 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,11 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',['only' => 'create']);
+    }
+
     public function create(){
        return view('sessions.create');
     }
@@ -15,7 +20,7 @@ class SessionsController extends Controller
         $credentials=$this->validate($request,['email'=>'required|email|max:255','password'=>'required']);
         if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','欢迎回来');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         }else{
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
