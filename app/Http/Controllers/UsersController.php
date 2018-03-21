cb001a2143c60d2dfbdc0cdcab88d032f812fded
luozong <it004@ferex.com.cn>
2018-03-21 07:21:26 +0000

    Add user statuses

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,8 @@ class UsersController extends Controller
         return view('users.create');
     }
     public function show(User $user){
-        return view('users.show',compact('user'));
+        $statuses=$user->statuses()->orderBy('created_at','desc')->paginate(30);
+        return view('users.show',compact('user','statuses'));
     }
     public function store(Request $request){
         $this->validate($request,[
